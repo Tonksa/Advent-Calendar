21f897b0ef5436383700d30ec56d194e9d81dde3
Alex Tonkins <alextonkins@hotmail.co.uk>
2024-01-12 21:38:09 +0000

    add placeholder images and text - now ready for selling

--- src/js/json/slot_data.js
+++ src/js/json/slot_data.js
@@ -2,145 +2,145 @@ export let slot_data = [
     {
         "id": 1,
         "type": "image",
-        "content": "Let's unlock some memories...",
-        "image": require('../../images/1.JPG')
+        "content": "Unlocking memories that warm the heart.",
+        "image": require('../../images/1.jpg')
     },
     {
         "id": 2,
         "type": "image",
-        "content": "Think we've changed a bit?",
+        "content": "Seeing our journey unfold - timeless moments.",
         "image": require('../../images/2.jpg')
     },
     {
         "id": 3,
         "type": "image",
-        "content": "Forever impressed with how well we scrub up.",
-        "image": require('../../images/3.JPG')
+        "content": "Always impressed by our ability to dress up.",
+        "image": require('../../images/3.jpg')
     },
     {
         "id": 4,
         "type": "image",
-        "content": "The definition of \"in his element\".",
+        "content": "In his element.",
         "image": require('../../images/4.jpg')
     },
     {
         "id": 5,
         "type": "image",
-        "content": "Moments before the iconic \"sweets\" tantrum.",
+        "content": "He still pulls that face now!",
         "image": require('../../images/5.jpg')
     },
     {
         "id": 6,
         "type": "image",
-        "content": "Number 6 for how many years Rebecca has been killing it in Dubai!",
-        "image": require('../../images/6.jpeg')
+        "content": "Here's to us, exploring new horizons together.",
+        "image": require('../../images/6.jpg')
     },
     {
         "id": 7,
         "type": "image",
-        "content": "Disneyland, Egypt, Turkey, Lanzarote, Dubai x2, Budapest... The 7 holidays we've had together.",
+        "content": "Magical Holidays - memories we'll never forget.",
         "image": require('../../images/7.jpg')
     },
     {
         "id": 8,
         "type": "image",
-        "content": "Some of the best times at Centre Parcs...",
+        "content": "Laughs and joy wherever we went!",
         "image": require('../../images/8.jpg')
     },
     {
         "id": 9,
         "type": "image",
-        "content": "The old man and I at Anfield, up the bloody reds!",
+        "content": "Celebrating Mother's Day - we love you.",
         "image": require('../../images/9.jpg')
     },
     {
         "id": 10,
         "type": "image",
-        "content": "10 for Mothers Day, and the worlds best mum.",
+        "content": "Pure nostalgia.",
         "image": require('../../images/10.jpg')
     },
     {
         "id": 11,
         "type": "image",
-        "content": "Now THAT'S a throwback.",
+        "content": "Against all odds, we did it!",
         "image": require('../../images/11.jpg')
     },
     {
         "id": 12,
         "type": "image",
-        "content": "She only went and bloody did it!",
+        "content": "Remember the golfing phase?",
         "image": require('../../images/12.jpg')
     },
     {
         "id": 13,
         "type": "image",
-        "content": "Prom was 10 years ago...",
-        "image": require('../../images/13.JPG')
+        "content": "She always was the favourite Babysitter...",
+        "image": require('../../images/13.jpg')
     },
     {
         "id": 14,
         "type": "image",
-        "content": "Eleanor had a Michael Jackson phase??",
-        "image": require('../../images/14.JPG')
+        "content": "We're all going to the Festival next year!",
+        "image": require('../../images/14.jpg')
     },
     {
         "id": 15,
         "type": "image",
-        "content": "Janine was a very fun babysitter...",
-        "image": require('../../images/15.JPG')
+        "content": "Aww, he's the best.",
+        "image": require('../../images/15.jpg')
     },
     {
         "id": 16,
         "type": "image",
-        "content": "16 for Fathers Day, and the best dad there ever was.",
+        "content": "Good friends, even better food!",
         "image": require('../../images/16.jpg')
     },
     {
         "id": 17,
         "type": "image",
-        "content": "Dom, our best boy.",
+        "content": "Love, love, LOVE the Snow.",
         "image": require('../../images/17.jpg')
     },
     {
         "id": 18,
         "type": "image",
-        "content": "A quick reminder of Mum's dark hair phase...",
-        "image": require('../../images/18.JPG')
+        "content": "Our favourite place to walk!",
+        "image": require('../../images/18.jpg')
     },
     {
         "id": 19,
         "type": "image",
-        "content": "Cheeky snow day!",
-        "image": require('../../images/19.JPG')
+        "content": "Remember this action shot?!",
+        "image": require('../../images/19.jpg')
     },
     {
         "id": 20,
         "type": "image",
-        "content": "Jingle All The Way ❤️",
-        "image": require('../../images/20.JPG')
+        "content": "Now that belongs on vogue.",
+        "image": require('../../images/20.jpg')
     },
     {
         "id": 21,
         "type": "image",
-        "content": "Glastonbury 2019.",
-        "image": require('../../images/21.JPG')
+        "content": "Definitely getting too old for this!",
+        "image": require('../../images/21.jpg')
     },
     {
         "id": 22,
         "type": "image",
-        "content": "Straight out of vogue.",
-        "image": require('../../images/22.JPG')
+        "content": "Grateful for your endless love and support.",
+        "image": require('../../images/22.jpg')
     },
     {
         "id": 23,
         "type": "image",
-        "content": "Day 23, for the day of the best people's wedding anniversary.",
-        "image": require('../../images/23.JPG')
+        "content": "Celebrating us on a special wedding anniversary.",
+        "image": require('../../images/23.jpg')
     },
     {
         "id": 24,
         "type": "image",
-        "content": "Mum & Dad, thanks for everything you do for us every year. Love you always, Merry Christmas x",
+        "content": "Merry Christmas filled with love, joy, and cherished memories.",
         "image": require('../../images/24.jpg')
     }
-]
+];
